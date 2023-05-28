ee916eaacdf90746ea5c03799614d89f3e2e8914
imalicx <xenrecords@yahoo.com>
2023-05-27 21:45:11 -0400

    updated changes

--- js/app.js
+++ js/app.js
@@ -4,6 +4,3 @@ const navLink = document.querySelector('.nav__link');
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide');
 });
-
-git clone https://github.com/imalicx/EnjoyTheOutdoors2.git
-
